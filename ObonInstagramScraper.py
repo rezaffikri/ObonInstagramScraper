0157d3a200f0b9e670d88e347dc8aa56fb5ef2fe
Reza F. Fikri <rezaffikri@outlook.com>
2022-04-04 05:31:39 +0700

    add appsetting.json update minor get media

--- ObonInstagramScraper.py
+++ ObonInstagramScraper.py
@@ -1,4 +1,5 @@
 import shutil
+from socket import timeout
 import instaloader
 import telegram_send
 import time
@@ -6,12 +7,35 @@ import datetime
 import os
 import platform
 import random
-
-present = datetime.datetime.now()
-path = os.getcwd() + "/MediaDownloads/"
+import json
 
 print("Starting")
 L = instaloader.Instaloader(download_comments=False, max_connection_attempts=9, post_metadata_txt_pattern=None, save_metadata=False, download_video_thumbnails=False, download_geotags=False, filename_pattern="{shortcode}")
+
+present = datetime.datetime.now()
+folderDownload = "MediaDownloads/"
+path = os.getcwd() + "/"  + folderDownload
+
+if os.path.exists("AppSettings.json"):
+    with open('AppSettings.json', 'r') as f:
+        config = json.load(f)
+        instagramUserName = config["instagram"]["username"]
+        instagramPassword = config["instagram"]["password"]
+        telegramToken = config["telegram_send"]["token"]
+        telegramChatId = config["telegram_send"]["chat_id"]
+
+        if telegramToken and telegramChatId:
+            path_config = telegram_send.get_config_path()
+            with open(path_config, 'w') as f:
+                f.write(f'[telegram]\ntoken = {telegramToken}\nchat_id = {telegramChatId}')
+                print("Override telegram-send config")       
+        if instagramUserName and instagramPassword:
+            print("Login")
+            BOT_INSTAGRAM_ACCOUNT = L.login(instagramUserName,instagramPassword)
+            print("Login successful")
+else:
+    # assume you are not want to login and already config telegram-send from command line with telegram-send --configure-group
+    print("Not login")
 
 # if you want to download private user media, you need to login and follow their instagram
 # if your network has been restricted, you need to login too, or you have to wait before hit again and i don't know how long
@@ -23,7 +47,7 @@ L = instaloader.Instaloader(download_comments=False, max_connection_attempts=9, 
 PROFILES = ["muslimmyway", "masjidnuruliman", "muhajirprojectpeduli", "muhajirproject", "ad_dariny", "eta.erwanditarmiziofficial", "muhajirprojecttilawah", "muslimafiyahcom", "the_rabbaanians", "haloustadz", "dzulqarnainms", "ustadzaris", "yufid.tv", "parentingruqoyyah", "ub_cintasunnah", "mabduhtuasikal", "hsi.abdullahroy", "rodjatv", "firanda_andirja_official", "rumayshocom", "manhajsalafus.shalih", "indonesiabertauhidofficial", "khalidbasalamahofficial", "amminurbaits", "muslimorid", "muhammadnuzuldzikri", "fikihmuamalatkontemporer", "raehanul_bahraen", "syafiqrizabasalamah_official"]
 
 # Dev
-# PROFILES = ["masjidnuruliman"]
+# PROFILES = ["fikihmuamalatkontemporer"]
 
 while True:
     try:
@@ -31,37 +55,48 @@ while True:
             L.dirname_pattern = path + PROFILE
             print("Profile: "+PROFILE)
             print("Timeout: random between 31 and 1800 seconds")
-            # time.sleep(random.randint(31,1800))
+            time.sleep(random.randint(31,1800))
             profile = instaloader.Profile.from_username(L.context, PROFILE)
             print("Profile loaded")
             for post in profile.get_posts():
                 #get today post only
-                if post.date.date() == datetime.datetime.now().date():
+                if post.date_local.date() == datetime.datetime.now().date():
                     print("Timeout: random between 31 and 1800 seconds")
-                    # time.sleep(random.randint(31,1800))
+                    time.sleep(random.randint(31,1800))
                     download = L.download_post(post,PROFILE)
+                    folderProfile = folderDownload + post.owner_username
                     if download == True:
-                        loopId = 0      
-                        for slide in post.get_sidecar_nodes():
-                            try:
-                                loopId = loopId + 1
-                                fileId = ""
-                                if post.mediacount >= 2:
+                        loopId = 0
+                        if post.mediacount >= 2:
+                            for slide in post.get_sidecar_nodes():
+                                try:
+                                    loopId = loopId + 1
                                     fileId = "_"+str(loopId)
-                                if not slide.is_video:
-                                    with open(post.owner_username+"/"+post.shortcode+fileId+".jpg", "rb") as f:
-                                        telegram_send.send(images=[f], timeout=240) 
+                                    if not slide.is_video:
+                                        with open(folderProfile+"/"+post.shortcode+fileId+".jpg", "rb") as f:
+                                            telegram_send.send(images=[f]) 
+                                    else:
+                                        with open(folderProfile+"/"+post.shortcode+fileId+".mp4", "rb") as f:
+                                            telegram_send.send(files=[f])   
+                                except ValueError:
+                                    print("Send media error")
+                                    pass
+                        else:
+                            try:
+                                if not post.is_video:
+                                    with open(folderProfile+"/"+post.shortcode+".jpg", "rb") as f:
+                                        telegram_send.send(images=[f]) 
                                 else:
-                                    with open(post.owner_username+"/"+post.shortcode+fileId+".mp4", "rb") as f:
-                                        telegram_send.send(files=[f], timeout=240)   
-                            except:
+                                    with open(folderProfile+"/"+post.shortcode+".mp4", "rb") as f:
+                                        telegram_send.send(files=[f])   
+                            except ValueError:
                                 print("Send media error")
                                 pass
                         try:
                             if post.caption is None:
-                                telegram_send.send("None")
+                                telegram_send.send(messages=[""+post.owner_username+": None"])
                             else:
-                                telegram_send.send(post.owner_username+": "+post.caption)
+                                telegram_send.send(messages=[post.owner_username+": "+post.caption])
                         except:
                             print("Send caption error")
                             pass
@@ -91,6 +126,7 @@ while True:
                 #                 os.remove(g)
                 #                 print("File deleted")
                 shutil.rmtree(path)
+                print("Old file deleted")
             except:
                 print("Delete old file error")  
     except ValueError:
